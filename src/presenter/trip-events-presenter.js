b15eaa8c012ee910eec3b3ce4ae2e12b3b91d916
Irina Trubchik <trubchik@mail.ru>
2022-05-02 01:19:54 +0300

    добавлены скрипты для view и presenter

---
+++ src/presenter/trip-events-presenter.js
@@ -0,0 +1,17 @@
+import SortView from '../view/sort-view.js';
+import RouteEventsListView from '../view/route-events-list-view.js';
+import RouteEventView from '../view/route-event-view.js';
+import EventListItemView from '../view/event-list-item-view.js';
+import EventCreationFormView from '../view/create-event-form-view.js';
+import EventDetailsView from '../view/events-details.js';
+import EventOffersView from '../view/event-offers-view.js';
+import EventDestinationView from '../view/event-destination-view.js';
+import TripInfoView from '../view/trip-info-view.js';
+import {
+  render
+} from '../render.js';
+import {
+  RenderPosition
+} from '../render.js';
+
+const FIRST_EVENTS_QUANTITY = 3;
