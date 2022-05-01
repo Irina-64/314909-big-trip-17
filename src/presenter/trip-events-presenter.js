7a7df587c72113ef8c6968b2c8c7a75a7b144719
Irina Trubchik <trubchik@mail.ru>
2022-05-02 01:41:51 +0300

    внесены изменения в trip-events-presenter

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -15,3 +15,31 @@ import {
 } from '../render.js';
 
 const FIRST_EVENTS_QUANTITY = 3;
+
+export default class TripEventsPresenter {
+  sortComponent = new SortView();
+  routeEventsListComponent = new RouteEventsListView();
+  eventListItem = new EventListItemView();
+  eventCreationFormComponent = new EventCreationFormView();
+  eventDetailsComponent = new EventDetailsView();
+
+
+  init = (tripEventsContainer, tripHeaderContainer) => {
+    this.tripEventsContainer = tripEventsContainer;
+    this.tripHeaderContainer = tripHeaderContainer;
+    render(this.sortComponent, this.tripEventsContainer);
+    render(this.routeEventsListComponent, this.tripEventsContainer);
+    render(this.eventListItem, this.routeEventsListComponent.getElement());
+    render(this.eventCreationFormComponent, this.eventListItem.getElement());
+    render(this.eventDetailsComponent, this.eventCreationFormComponent.getElement());
+    render(new EventOffersView(), this.eventDetailsComponent.getElement());
+    render(new EventDestinationView(), this.eventDetailsComponent.getElement());
+
+    for (let i = 0; i < FIRST_EVENTS_QUANTITY; i++) {
+      render(this.eventListItem, this.routeEventsListComponent.getElement());
+      render(new RouteEventView(), this.eventListItem.getElement());
+    }
+
+    render(new TripInfoView(), tripHeaderContainer, RenderPosition.AFTERBEGIN);
+  };
+}
