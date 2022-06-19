e82146c1008dcd59c78f2c9a53359a546ee8468f
Trira <trubchik@mail.ru>
2022-06-19 22:52:20 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 
 import FilterView from './view/filter-view.js';
 import { render } from './render.js';
-import {
-  RenderPosition
-} from './render.js';
+import { RenderPosition } from './render.js';
 
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
-
 
 const siteMainElement = document.querySelector('.page-main');
 const siteHeaderMainElement = document.querySelector('.trip-main');
@@ -15,9 +12,7 @@ const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
 const siteHeaderElement = document.querySelector('.page-header');
 const siteTripControlsFilters = siteHeaderElement.querySelector('.trip-controls__filters');
 
-
 render(new FilterView(), siteTripControlsFilters, RenderPosition.BEFOREEND);
-
 
 const tripEventsPresenter = new TripEventsPresenter();
 
