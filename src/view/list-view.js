9c5c656a9258af6484d5f0eee6c0b4f4b66392d7
Trira <trubchik@mail.ru>
2022-06-22 03:07:03 +0300

    Update list-view.js

--- src/view/list-view.js
+++ src/view/list-view.js
@@ -1,6 +1,6 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createRouteEventsListTemplate = () => '<ul class="trip-events__list"></ul>';
+const createNewListTemplate = () => '<ul class="trip-events__list"></ul>';
 
 export default class ListView extends AbstractView {
 
