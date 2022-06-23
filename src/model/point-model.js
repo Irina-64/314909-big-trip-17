6f81b4cf52be425885d8843edfb3674183a6d75f
Irina Trubchik <trubchik@mail.ru>
2022-06-24 00:43:39 +0300

    исправления в функции deletePoint

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -70,18 +70,9 @@ export default class PointModel extends Observable {
   };
 
   deletePoint = async (updateType, update) => {
-    const index = this.#points.findIndex((point) => point.id === update.id);
-
-    if (index === -1) {
-      throw new Error('Can\'t delete un-existing point');
-    }
-
     try {
       await this.#pointsApiService.deletePoint(update);
-      this.#points = [
-        ...this.#points.slice(0, index),
-        ...this.#points.slice(index + 1),
-      ];
+      this.#points = [...this.#points.filter((point) => point.id !== update.id)];
       this._notify(updateType);
     } catch (err) {
       throw new Error('Can\'t delete point');
